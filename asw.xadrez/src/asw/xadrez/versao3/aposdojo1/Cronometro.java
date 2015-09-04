3bbe0d099e4a12796cc76e9c65d51879c9f0c0f3
renatosrib <renatosrib@gmail.com>
2015-09-04 19:20:47 -0300

    Evolução da classe cronometro

--- asw.xadrez/src/asw/xadrez/versao3/aposdojo1/Cronometro.java
+++ asw.xadrez/src/asw/xadrez/versao3/aposdojo1/Cronometro.java
@@ -3,6 +3,7 @@ package asw.xadrez.versao3.aposdojo1;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.JLabel;
@@ -14,35 +15,46 @@ public class Cronometro {
 	private final int timeInSeconds;
 	private final static int DEFAULT_TIME_IN_SECONDS = 300; 
 	
-	
+	private JLabel tempo = new JLabel("Aguarde...");
+	private Timer timer;
 	
 	
 	public Cronometro(int timeInSeconds){
 		this.timeInSeconds = timeInSeconds;
-		Timer timer = new Timer(0, new ActionListener() {
+		 timer = new Timer(0, new ActionListener() {
 			int temp = timeInSeconds;
+			SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");		
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Date updatedTime = null;				
 				
 				try {
 					updatedTime = (Date) dateFormat.parse(String.valueOf(temp/3600+":"+temp/60+":"+temp%60));
+					tempo.setText(dateFormat.format(updatedTime));
+					Thread.sleep(1000);
+					Thread.sleep(1000);
 				} catch (ParseException e2) {
-					// TODO Auto-generated catch block
 					e2.printStackTrace();
 				}
-				tempo.setText(dateFormat.format(updatedTime));	
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
+				catch (InterruptedException e1) {
 					e1.printStackTrace();
 				}
-				temp ++;
+				temp--;
 			}
 		}); 
 	}
 	
+	public JLabel getTempoLabel(){
+		return tempo;
+	}
+	
+	public void start(){
+		timer.start();
+	}
+	
+	public void stop(){
+		timer.stop();
+	}
 	
 
 }
