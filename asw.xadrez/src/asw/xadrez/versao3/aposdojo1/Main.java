6dcdef506271d15b7a697a3c9aab08cedd68c851
renatosrib <renatosrib@gmail.com>
2015-09-09 00:45:17 -0300

    Mudança de estratégia para implementar a funcionalidade

--- asw.xadrez/src/asw/xadrez/versao3/aposdojo1/Main.java
+++ asw.xadrez/src/asw/xadrez/versao3/aposdojo1/Main.java
@@ -1,20 +1,16 @@
 package asw.xadrez.versao3.aposdojo1;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.Timer;
+import javax.swing.JPanel;
 
-public class Main{
-	static int time = 0;
+public class Main {
+
 	static final Color COR_CASA_SELECIONADA = Color.CYAN;
 	static final Color COR_CASA_NAO_SELECIONADA = new Color(120, 120, 120);
 
@@ -22,75 +18,54 @@ public class Main{
 	private static final int ALTURA_EM_PIXELS = 256;
 	
 	static final String CASA_VAZIA = "_";
-	
-	public void trocarCronometro(){
-		
-	}
-	
 
-	public static void main(String[] args) throws InterruptedException {
-		JLabel tempo;
-		Tabuleiro tabuleiro = new Tabuleiro();		
-		Cronometro cronometro = new Cronometro(500);
+	public static void main(String[] args) {
+		Tabuleiro tabuleiro = new Tabuleiro();
 		
-		JFrame janela = criarJanela();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-		
-		janela.setSize(300, 400);
-		janela.setVisible(true);
+		JFrame janela = criarJanela();	
 		
 		
-		
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//JLabel tempo2 = new JLabel(String.valueOf(15));
-/*		janela.add(tempo2);
-	 
-*/
-		
+
 		MouseAdapter tratadorCliques = new TratadorCliques();
-		
-		preencherJanelaComCasas(tabuleiro, janela, tratadorCliques);
-		
-		exibirJanela(janela);
-				
-		
 
+		preencherJanelaComCasas(tabuleiro, janela, tratadorCliques);
+		JPanel controle = new JPanel();
+		controle.setSize(new Dimension(256, 40));
+		janela.add(controle);
+
+		exibirJanela(janela);
 	}
 
 		private static JFrame criarJanela() {
-		JFrame janela = new JFrame("Xadrez");		
+			
+		JFrame janela = new JFrame("Xadrez");
+				
 		janela.setLayout(new GridLayout(Constantes.NUMERO_LINHAS_TABULEIRO, Constantes.NUMERO_COLUNAS_TABULEIRO));
-		janela.setSize(LARGURA_EM_PIXELS, ALTURA_EM_PIXELS);		
+		janela.setSize(LARGURA_EM_PIXELS, ALTURA_EM_PIXELS);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		return janela;
 	}
 
 	private static void preencherJanelaComCasas(Tabuleiro tabuleiro, JFrame janela,
 			MouseAdapter tratadorCliques) {
-		
-		JLabel timeRemain = new JLabel("teste");
 		for (int i = 0; i < Constantes.NUMERO_LINHAS_TABULEIRO; ++i) {
 			for (int j = 0; j < Constantes.NUMERO_COLUNAS_TABULEIRO; ++j) {
 				Peca peca = tabuleiro.getPeca(i, j);
-				
 				JLabel label = new JLabel(peca.toString());
+				label.setSize(32, 32);
 				label.setForeground(peca.getCor() == Cor.INDEFINIDO ? Color.CYAN:peca.getCor() == Cor.PRETO ? Color.BLACK : Color.WHITE);
 				label.setBackground(COR_CASA_NAO_SELECIONADA);
 				label.setHorizontalAlignment(JLabel.CENTER);
 				label.setVerticalAlignment(JLabel.CENTER);
 				label.setOpaque(true);
-				label.addMouseListener(tratadorCliques);						
+				label.addMouseListener(tratadorCliques);
+				janela.add(label);
 			}
 		}
-		janela.add(timeRemain);
 	}
 
 	private static void exibirJanela(JFrame janela) {
 		janela.setLocationRelativeTo(null);
 		janela.setVisible(true);
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		String string  = "teset";
-		string.length();
 	}
 }
